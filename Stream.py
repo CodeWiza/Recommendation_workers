f56ec11d543bb346f87360db56c54ea937f21a70
Sourabh singh <96938361+SouSingh@users.noreply.github.com>
2024-05-13 10:37:24 +0530

    Update Stream.py

--- Stream.py
+++ Stream.py
@@ -16,6 +16,7 @@ from langchain_community.chat_models import ChatCohere
 from langchain_openai import ChatOpenAI
 import streamlit as st
 from loguru import logger as log
+from streamlit_option_menu import option_menu
 from llama_index.program.openai import OpenAIPydanticProgram
 from llama_index.core import ChatPromptTemplate
 from llama_index.core.llms import ChatMessage
@@ -33,16 +34,17 @@ def set_api_keys():
 
 
 set_api_keys()
+
 search_tool = SerperDevTool()
-web_rag_tool = WebsiteSearchTool()
 
 
 def create_search_agent():
+    web_rag_tool = WebsiteSearchTool()
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
     return Agent(
         role='Researcher',
-        goal='Conduct foundational research of product {topic} and specified country {topic} and company must be public-government sector if list not full fill then search top private company also and strict find Review, email, phone-number',
-        backstory="""You a top skilled researcher and can easily find company name of product and list all of them in form of list and return JSON file as having heading like 1.Introducation of product 2.Overview of product 3. Top supplier in India according to number user Asked and return Company_name, Company_websitelink, company_country ,summary,product offerings(company all product details),Reviews, Email(atleast 2 emails-ids), Phone_number about company 4.Conculsion and 5.Reference. Very Important details needed => Reviews, Email(atleast 2 emails-ids), Phone_number about company """,
+        goal='Conduct foundational research of product {topic} and specified country {topic} and company must be public-government sector if list not full fill then search top private company also and strict find Review, email, phone-number(should be integer)',
+        backstory="""You a top skilled researcher and can easily find company name of product and list all of them in form of list and return JSON file as having heading like 1.Introducation of product 2.Overview of product 3. Top supplier in India according to number user Asked and return Company_name, Company_websitelink, company_country ,summary,product offerings(company all product details),Reviews(Search on top 5 Social media website and list them with name of each about review), Email(atleast 2 emails-ids), Phone_number about company(compulsory should be integer) 4.Conculsion and 5.Reference. Very Important details needed => Reviews, Email(atleast 2 emails-ids), Phone_number about company """,
         verbose=True,
         allow_delegation=False,
         llm = llm,
@@ -52,6 +54,7 @@ def create_search_agent():
 
 
 def run_search_task(agent, result):
+    web_rag_tool = WebsiteSearchTool()
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
     Search_task = Task(
         description=("Gather relevant data of product and company."),
@@ -62,7 +65,7 @@ def run_search_task(agent, result):
         agents=[agent],
         tasks=[Search_task],
         verbose=10,
-        manager_llm=llm,
+        manager_llm = llm,
         process=Process.sequential,
         full_output=True
     )
@@ -70,6 +73,7 @@ def run_search_task(agent, result):
 
 
 def run_financial_analysis(topic):
+    web_rag_tool = WebsiteSearchTool()
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
     finance_Analyser = Agent(
@@ -101,11 +105,11 @@ def run_financial_analysis(topic):
 
 def run_writer_task(topic):
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-
+    web_rag_tool = WebsiteSearchTool()
     finance_writer = Agent(
         role='Writer',
-        goal='Conduct foundational research of little news for each part of news analyser data of 1) Legal issues about a supplier(find all issue and write in points with summary)  2) who are board members of the company (write in points) 3) Any issues with the board member (All issue and write in points) 4) financial wrong doing of the company (All financial wrong doing and summarize in points) 5) Labour Strike (All strike and combine into points) 6) Refernce  and gather less data for all points | company_name => {topicd}',
-        backstory="""You a top skilled expert resaerch and can easily find 1) Legal issues about a supplier(find all issue and write in points with summary)  2) who are board members of the company (write in points) 3) Any issues with the board member (All issue and write in points) 4) financial wrong doing of the company (All financial wrong doing and summarize in points) 5) Labour Strike (All strike and combine into points) 6) Refernce for with link and U can find Easily Fast and Accurate details and u search only news data website""",
+        goal='Conduct foundational research of {topicd} for each part of news for Legal issues about a supplier,board members of the supplier,issues with the board member,financial wrong of the company,Labour Strike,Refernce and gather all data company_name => {topicd}',
+        backstory="""You a top skilled expert resaerch and can easily find Legal issues about a supplier,board members of the supplier,issues with the board member, financial wrong doing of the company,Labour Strike,Refernce with link you search only news website""",
         verbose=True,
         allow_delegation=False,
         llm = llm,
@@ -114,8 +118,8 @@ def run_writer_task(topic):
     )
 
     finance_writing_task = Task(
-        description=('Analyze the data of company and return 1)Legal-issues 2)Board-Member 3)Issues Board-member (All issue and write in points) 4)Financial-Wrong (All financial wrong doing and summarize in points) 5)Labour-Strike (All strike and combine into points) 6)Refernce  and output should be JSON format and strict print data for all company in Json format'),
-        expected_output='Gather relevant data and list all of them in form of list and return JSON only data and having heading like 1)Legal-issues 2)Board-Member 3)Issues Board-member (All issue and write in points) 4)Financial-Wrong (All financial wrong doing and summarize in points) 5)Labour-Strike (All strike and combine into points) 6)Refernce  with link of all company name of input . Return all points in Json type and accurate and beautiful view and heading for each json should be same no spacing noting',
+        description=('Analyze the data of company for Legal-issuee,Board-Member,Issues Board-member,Financial-Wrong,Labour-Strike,Refernce'),
+        expected_output='Gather relevant data for Legal-issues,Board-Member,Issues Board-member,Financial-Wrong,Labour-Strik,Refernce with link',
         output_file=f"3.json", agent=finance_writer
     )
 
@@ -161,7 +165,7 @@ def extract_information_from_company(transcript):
         "Overview of Product": "Overview of Product",
         "Top Suppliers": ["Company Name", "Website", "Country", "Summary", "Product Offerings", "Reviews", "Emails", "Phone Numbers"],
         "Conclusion": "A conclusion summarizing the reliability of the listed companies",
-        "References": "A list of references related to the information provided.",
+        "References": "A list of references related to the information provided. and strict must be link",
     }
 
     json_example = json.dumps(dict_example)
@@ -241,11 +245,11 @@ def extract_finance_information_from_text(transcript):
 
 
     dict_example = {
-        "Company" : ["Company 1", "Company 2"],
-        "Financial Information" : ["atleast 5 big article for Financial_highlights Company 1","atleast 5 big article for Financial_highlights Company 2"],
-        "Positives": ["atleast 5 big article points for Positive of company 1", "atleast 5 big article points for Positive of company 2"],
-        "Key Concerns": ["atleast 5 key points of company 1", "atleast 5 key points of company 2"],
-        "References": ["all link realted to company 1", "all link realted to company 2"]
+        "Company" : "Company information",
+        "Financial Information" : "atleast 5 big article for Financial_highlights Company ",
+        "Positives": "atleast 5 big article points for Positive of company ",
+        "Key Concerns": "atleast 5 key points of company ",
+        "References": "all link realted to companyand strict must be link ",
     }
 
     json_example = json.dumps(dict_example)
@@ -284,13 +288,13 @@ def extract_news_information_from_text(transcript):
 
 
     dict_example = {
-        "Company" : ["Company 1", "Company 2"],
-        "Legal Issues" : ["atleast 5 big article points for Legal Issues Company 1 with ","atleast 5 big article points for Legal Issues Company 2"],
-        "Board Members": ["Board Members of company 1", "Board Members of company 2"],
-        "Issues with Board Members": ["atleast 5 key points Issues with Board Members of company 1", "atleast 5 key points Issues with Board Members of company 2"],
-        "Financial Wrongdoing": ["Financial Wrongdoing of company 1", "Financial Wrongdoing of company 2"],
-        "Labour Strikes": ["atleast 5 key points Labour Strikes of company 1", "atleast 5 key points Labour Strikes of company 2"],
-        "References": ["all link realted to company 1", "all link realted to company 2"]
+        "Company" : "Company 1",
+        "Legal Issues" : "atleast 5 big article points for Legal Issues Company ",
+        "Board Members": "Board Members of company ",
+        "Issues with Board Members": "atleast 5 key points Issues with Board Members of company ", 
+        "Financial Wrongdoing": "Financial Wrongdoing of company ",
+        "Labour Strikes": "atleast 5 key points Labour Strikes of company ",
+        "References": "all link realted to company ",
     }
 
     json_example = json.dumps(dict_example)
@@ -405,85 +409,112 @@ def display_product_info(product_data):
         st.write(f"[References]({product_data['References']})")
 
 
-def display_company_info(company_data):
+def display_company_finance_details(company_json):
+    # Parse the JSON string into a Python dictionary
+    company_data = json.loads(company_json)
+
     # Display company name
     if "Company" in company_data:
-        st.subheader("Company")
-        st.write(", ".join(company_data["Company"]))
+        st.subheader("Company Name")
+        st.write(company_data["Company"])
 
     # Display financial information
     if "Financial Information" in company_data:
         st.subheader("Financial Information")
-        for info in company_data["Financial Information"]:
-            st.write(info)
+        financial_info = company_data["Financial Information"]
+        st.write(financial_info)
 
     # Display positives
     if "Positives" in company_data:
         st.subheader("Positives")
-        for positive in company_data["Positives"]:
-            st.write("- " + positive)
+        positives = company_data["Positives"]
+        st.write(positives)
 
     # Display key concerns
     if "Key Concerns" in company_data:
         st.subheader("Key Concerns")
-        for concern in company_data["Key Concerns"]:
-            st.write("- " + concern)
+        key_concerns = company_data["Key Concerns"]
+        st.write(key_concerns)
 
     # Display references
     if "References" in company_data:
         st.subheader("References")
-        for ref in company_data["References"]:
-            st.write(f"[{ref}]({ref})")
+        references = company_data["References"]
+        st.write(references)
 
 
-def display_company_details(company_data):
+def display_company_news_details(company_json):
+    # Parse the JSON string into a dictionary
+    company_data = json.loads(company_json)
+
     # Display company name
     if "Company" in company_data:
-        st.subheader("Company")
-        st.write(", ".join(company_data["Company"]))
+        st.subheader("Company Name")
+        st.write(company_data["Company"])
 
     # Display legal issues
     if "Legal Issues" in company_data:
         st.subheader("Legal Issues")
-        for issue in company_data["Legal Issues"]:
-            st.write("- " + issue)
+        st.write(company_data["Legal Issues"])
 
     # Display board members
     if "Board Members" in company_data:
         st.subheader("Board Members")
-        st.write(company_data["Board Members"])
+        board_members = company_data["Board Members"]
+        if isinstance(board_members, list) and len(board_members) > 0:
+            for member in board_members:
+                st.write(f"- {member}")
+        else:
+            st.write("No specific board members mentioned.")
 
     # Display issues with board members
     if "Issues with Board Members" in company_data:
         st.subheader("Issues with Board Members")
-        for issue in company_data["Issues with Board Members"]:
-            st.write("- " + issue)
+        st.write(company_data["Issues with Board Members"])
 
     # Display financial wrongdoing
     if "Financial Wrongdoing" in company_data:
         st.subheader("Financial Wrongdoing")
-        for issue in company_data["Financial Wrongdoing"]:
-            st.write("- " + issue)
+        st.write(company_data["Financial Wrongdoing"])
 
     # Display labour strikes
     if "Labour Strikes" in company_data:
         st.subheader("Labour Strikes")
-        for strike in company_data["Labour Strikes"]:
-            st.write("- " + strike)
+        st.write(company_data["Labour Strikes"])
 
     # Display references
     if "References" in company_data:
         st.subheader("References")
-        for ref in company_data["References"]:
-            st.write(f"[{ref}]({ref})") # Display as clickable link
+        references = company_data["References"]
+        if isinstance(references, list):
+            for ref in references:
+                if isinstance(ref, dict) and "title" in ref and "link" in ref:
+                    st.write(f"- [{ref['title']}]({ref['link']})")
+                elif isinstance(ref, str):
+                    st.write(f"- {ref}")
+        else:
+            st.write("No references available.")
 
 
 search_agent = create_search_agent()
-logo_and_title = True
-if logo_and_title:
-    head = st.columns([2, 3, 5, 5])
-    with head[1]:
-        st.image("logo.png", use_column_width=False, width=380)
+st.set_page_config(
+    page_title="Supplier Discovery App",
+    layout="wide",
+    initial_sidebar_state="expanded",  # Optional: expand the sidebar initially  # Set the theme to light
+)
+logo_path = "logo.png"
+def round_image(image_path, width):
+    st.markdown(
+        f'<style>img.rounded{{border-radius:50%;}}</style>',
+        unsafe_allow_html=True,
+    )
+    st.image(image_path, width=width, output_format='PNG')
+
+
+
+# Display rounded logo image
+round_image(logo_path, width=180)
+
 
 
 # Initialize chat history
@@ -496,7 +527,10 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 recco = []
-st.title("Supplier discovery")
+st.markdown(
+    f'<p style="font-size: 60px; font-weight: bold; text-align: center; color: #5F16EB;">Supplier Discovery</p>',
+    unsafe_allow_html=True
+)
 # React to user input
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
@@ -516,26 +550,37 @@ if prompt := st.chat_input("What is up?"):
     st.write("--------------------------------------------------------------------------------------------")
     # Run financial analysis
     with st.spinner("Running financial analysis..."):
-        finance_sun = run_financial_analysis(out)
-        recco.append(finance_sun)
-        data = extract_finance_information_from_text(finance_sun)
+        data_finance = []
+        print(out)
+        out = json.loads(out)
+        companies = out.get('Companies', [])
+        for company_name in companies:
+            print(company_name)
+            data_fine = run_financial_analysis(company_name)
+            data = extract_finance_information_from_text(data_fine)
+            data_finance.append(data)
+        recco.append(data_finance)
+        print(data_finance)
         st.header('Financial Details')
-        data = json.loads(data)
-        display_company_info(data)
-        print(data)
+        for idx, json_data in enumerate(data_finance):
+            display_company_finance_details(json_data)
         print("2-----------------------------------------------------------------------------")
         #print(data)
     st.write("--------------------------------------------------------------------------------------------")
 
     # Run news summary task
     with st.spinner("Getting news details..."):
-        news_sum = run_writer_task(out)
-        recco.append(news_sum)
-        json_data = extract_news_information_from_text(news_sum)
-        data = json.loads(json_data)
-        print("3-----------------------------------------------------------------------------")
+        news_summa = []
+        companies = out.get('Companies', [])
+        for company_name in companies:
+            data_fine = run_writer_task(company_name)
+            data = extract_news_information_from_text(data_fine)
+            news_summa.append(data)
+        recco.append(news_summa)
+        print(news_summa)
         st.header('News Details')
-        display_company_details(data)
+        for idx, json_data in enumerate(news_summa):
+            display_company_news_details(json_data)
     st.write("--------------------------------------------------------------------------------------------")
 
     with st.spinner("Running Recommdation Agent..."):
@@ -545,4 +590,4 @@ if prompt := st.chat_input("What is up?"):
             st.header('Recommendation')
             out = json.loads(out)
             display_company_recommendation(out)
-    # Add assistant response to chat history
+    # Add assistant response to chat histor
